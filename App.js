0d16d11e96fc5316dd50350a7847fb0e319b3000
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2020-11-30 13:35:46 +0000

    clean-up

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
+
 import React, { useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
